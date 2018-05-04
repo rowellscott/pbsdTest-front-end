23820bf51191f310e7283eed5df23933b4be8296
Scott Rowell <rowell.scott@gmail.com>
2018-05-03 23:31:05 -0400

    Implementing Edit Form

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ExpenseService } from "../../expense.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-edit",
@@ -14,32 +15,83 @@ export class EditComponent implements OnInit {
     editName: string;
     editAmount: number;
     editDescription: string;
-  } = {
-    editDate: null,
-    editCustomerName: "",
-    editProjectName: "",
-    editName: "",
-    editAmount: 0,
-    editDescription: ""
   };
 
-  customers: any;
-  customerId: string = "";
-  constructor(private myExpense: ExpenseService) {}
+  updatedExpense: Object = {};
+
+  customers: Array<Object>;
+  projects: Array<Object>;
+
+  constructor(
+    private myExpense: ExpenseService,
+    private myRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    //Get Customers' Names For Dropdown Menu
     this.myExpense.getCustomers().subscribe(
       customers => {
         this.customers = customers;
-        console.log(this.customers);
+        // console.log(this.customers);
       },
       () => {
         console.log("Error Getting Customers");
       }
     );
+    //Get Expense Details To Populate Into Form
+    this.myRoute.params.subscribe(params => {
+      this.myExpense.getExpense(params["id"]).subscribe(
+        expense => {
+          // console.log(expense);
+          this.expense = {
+            editDate: expense.Date,
+            editCustomerName: expense.CustomerName,
+            editProjectName: expense.ProjectId.Name,
+            editName: expense.Name,
+            editAmount: expense.Amount.$numberDecimal,
+            editDescription: expense.Description
+          };
+          this.loadProjects();
+          // console.log(this.expense);
+        },
+        () => {
+          console.log("Error Getting Expense");
+        }
+      );
+    });
   }
 
+  // Get Projects According to Selected Customer
   loadProjects() {
-    console.log(this.expense.editCustomerName);
+    // console.log(this.expense.editCustomerName);
+    let customerId = "";
+    // Get Customer Id for Selected Customer
+    this.customers.forEach(customer => {
+      if (customer.Name === this.expense.editCustomerName) {
+        customerId = customer._id;
+      }
+    });
+
+    //Get List of Projects From Api
+    this.myExpense.getProjects(customerId).subscribe(
+      projects => {
+        console.log(projects);
+        this.projects = projects;
+      },
+      () => {
+        console.log("Error Retrieving Projects");
+      }
+    );
+  }
+
+  editExpense() {
+    this.updatedExpense = {
+      editDate: this.expense.editDate,
+      editCustomerName: this.expense.editCustomerName,
+      editProjectName: this.expense.editProjectName,
+      editName: this.expense.editName,
+      editAmount: this.expense.editAmount,
+      editDescription: this.expense.editDescription
+    };
   }
 }
--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -6,20 +6,37 @@ import "rxjs/add/operator/map";
 @Injectable()
 export class ExpenseService {
   constructor(private myHttp: Http) {}
-  customers: any = [];
+
+  //Get All Listings
   getListings() {
     return this.myHttp.get(`${environment.apiBase}`).map(res => res.json());
   }
 
+  //Get All Customers
   getCustomers() {
     return this.myHttp
       .get(`${environment.apiBase}/customers`)
       .map(res => res.json());
   }
 
+  //Get All Projects for Specific Customer
   getProjects(customerId) {
     return this.myHttp
       .get(`${environment.apiBase}/projects/${customerId}`)
+      .map(res => res.json());
+  }
+
+  //Get Expense Details
+  getExpense(id) {
+    return this.myHttp
+      .get(`${environment.apiBase}/expense/${id}`)
+      .map(res => res.json());
+  }
+
+  //Update Expense
+  updateExpense(id, updates) {
+    return this.myHttp
+      .put(`${environment.apiBase}/edit/${id}`, updates)
       .map(res => res.json());
   }
 }
